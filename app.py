36ea570cb08692c74a0da3e5924658df44d53c8e
phamvanminh272 <phamvanminh272@gmail.com>
2021-05-28 22:51:08 +0700

    first push

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import (
+    Flask, render_template, request, redirect
+)
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home_page():
+    return render_template('home_page.html')
+
+
+@app.route('/save_words', methods=['POST'])
+def save_words():
+    viet = request.form['viet']
+    eng = request.form['eng']
+    with open('data/viet_eng_words.json', 'r+') as f:
+        data = json.loads(f.read())
+    engs = eng.split(',')
+    current_engs = data.get(viet, [])
+    for i in engs:
+        if i not in current_engs:
+            current_engs.append(i)
+    data.update({viet: current_engs})
+    with open('data/viet_eng_words.json', 'w') as f:
+        json.dump(data, f, ensure_ascii=False)
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run()
