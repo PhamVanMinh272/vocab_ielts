ecbc4c3aa063bd65cbd852b3c3d8f3e366698498
phamvanminh272 <phamvanminh272@gmail.com>
2021-05-29 11:15:38 +0700

    use base html

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-    return render_template('base.html')
+    return render_template('home.html')
 
 
 @app.route('/save_words', methods=['POST'])
