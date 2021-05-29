1c4205f3e6c044934ee8d780a3f6be764b5cb14e
phamvanminh272 <phamvanminh272@gmail.com>
2021-05-29 16:43:47 +0700

    valid the fields

--- app.py
+++ app.py
@@ -2,34 +2,42 @@ from flask import (
     Flask, render_template, request, redirect, flash
 )
 import json
+from forms import SaveWordsForm
+from exceptions import NotExistException
 
 app = Flask(__name__)
 app.secret_key = 'why_do_we_need_this?'
 
 @app.route('/', methods=['GET'])
 def home_page():
-    return render_template('home.html')
+    form = SaveWordsForm()
+    return render_template('home.html', form=form)
 
 
 @app.route('/save_words', methods=['POST'])
 def save_words():
     try:
-        viet = request.form['viet']
-        eng = request.form['eng']
+        viet_value = request.form['viet'].strip()
+        engs_value = request.form['engs'].strip()
+        if not viet_value or not engs_value:
+            raise NotExistException("The words are empty")
         with open('data/viet_eng_words.json', 'r+') as f:
             data = json.loads(f.read())
-        engs = eng.split(',')
-        current_engs = data.get(viet, [])
+        engs = engs_value.split(',')
+        current_engs = data.get(viet_value, [])
         for i in engs:
             if i and i not in current_engs:
                 current_engs.append(i)
-        data.update({viet: current_engs})
+        data.update({viet_value: current_engs})
         with open('data/viet_eng_words.json', 'w') as f:
             json.dump(data, f, ensure_ascii=False)
-        flash('The words {} - {} were saved successfully'.format(viet, eng), 'success')
+        flash('The words {} - {} were saved successfully.'.format(viet_value, engs_value), 'success')
+        return redirect('/')
+    except NotExistException as ex:
+        flash('Failed to save. The words are empty.', 'error')
         return redirect('/')
     except Exception as ex:
-        flash('The words {} - {} were failed to save'.format(viet, eng), 'error')
+        flash('Failed to save', 'error')
         return redirect('/')
 
 
