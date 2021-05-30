b966fb33c90b95834cd6a6630faa94bf766e35fe
phamvanminh272 <phamvanminh272@gmail.com>
2021-05-30 12:53:04 +0700

    handle previous next button

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,15 @@
 $(".start-lesson-btn").click(function(){
   $.getJSON("/learn_vocab", function(result){
-    var num = 0;
-    localStorage.setItem("num", num+1);
+    var no = 0;
+    localStorage.setItem("no", no);
     $.each(result, function(i, field){
       var data = [i, field];
-      localStorage.setItem(num, JSON.stringify(data));
-      console.log(num, field);
-      num = num + 1;
+      localStorage.setItem(no, JSON.stringify(data));
+      no = no + 1;
     });
-    localStorage.setItem("length", num);
+    $(".lesson-area").removeClass("disabled");
+    localStorage.setItem("length_viet_words", no-1);
+    manage_previous_next_btn(0);
     data = JSON.parse(localStorage.getItem(0))
     $(".viet-word").html(data[1]);
     $(".viet-word").attr('id', data[0])
@@ -16,13 +17,38 @@ $(".start-lesson-btn").click(function(){
 });
 
 $(".next-word").click(function(){
-  var num_str = localStorage.getItem("num");
-  // if (num == localStorage.getItem("length")) {
-    
-  // }
-  num = parseInt(num_str);
-  data = JSON.parse(localStorage.getItem(num))
+  var no_str = localStorage.getItem("no");
+  var no = parseInt(no_str);
+  no = no + 1;
+  localStorage.setItem("no", no);
+  manage_previous_next_btn(no);
+  data = JSON.parse(localStorage.getItem(no))
   $(".viet-word").html(data[1]);
   $(".viet-word").attr('id', data[0])
-  localStorage.setItem("num", num+1);
 });
+
+$(".previous-word").click(function(){
+  var no_str = localStorage.getItem("no");
+  var no = parseInt(no_str);
+  no = no - 1;
+  localStorage.setItem("no", no);
+  manage_previous_next_btn(no);
+  data = JSON.parse(localStorage.getItem(no))
+  $(".viet-word").html(data[1]);
+  $(".viet-word").attr('id', data[0])
+});
+
+function manage_previous_next_btn(no) {
+  if (no === 0) {
+    $(".previous-word").attr('disabled', true);
+    $(".next-word").attr('disabled', false);
+  } else if (no === parseInt(localStorage.getItem("length_viet_words"))) {
+    $(".previous-word").attr('disabled', false);
+    $(".next-word").attr('disabled', true);
+  } else {
+    $(".previous-word").attr('disabled', false);
+    $(".next-word").attr('disabled', false);
+  }
+};
+
+localStorage.clear();
