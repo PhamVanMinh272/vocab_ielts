43716243969ccde6055bad5e4ee90afb764b7c0b
phamvanminh272 <phamvanminh272@gmail.com>
2021-06-12 17:07:15 +0700

    add list call api

--- db/vocab_db_scripts.sql
+++ db/vocab_db_scripts.sql
@@ -1,3 +1,14 @@
+CREATE TABLE list (
+	list_id INTEGER PRIMARY KEY,
+	list_name TEXT NOT NULL,
+	inserted_time TEXT
+);
+
+CREATE TABLE class (
+	class_id INTEGER PRIMARY KEY,
+	class_name TEXT NOT NULL
+);
+
 CREATE TABLE viet_words (
 	viet_id INTEGER PRIMARY KEY,
 	viet_word TEXT NOT NULL,
@@ -8,6 +19,34 @@ CREATE TABLE eng_words(
     eng_id INTEGER PRIMARY KEY,
 	eng_word TEXT NOT NULL,
 	inserted_time TEXT
+);
+
+CREATE TABLE list_and_viet(
+    list_id INTEGER,
+	viet_id INTEGER,
+    PRIMARY KEY (list_id, viet_id),
+    FOREIGN KEY (list_id)
+        REFERENCES list (list_id)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION,
+    FOREIGN KEY (viet_id)
+        REFERENCES viet_words (viet_id)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION
+);
+
+CREATE TABLE class_and_viet(
+    class_id INTEGER,
+	viet_id INTEGER,
+    PRIMARY KEY (class_id, viet_id),
+    FOREIGN KEY (class_id)
+        REFERENCES class (class_id)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION,
+    FOREIGN KEY (viet_id)
+        REFERENCES viet_words (viet_id)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION
 );
 
 CREATE TABLE viet_eng(
